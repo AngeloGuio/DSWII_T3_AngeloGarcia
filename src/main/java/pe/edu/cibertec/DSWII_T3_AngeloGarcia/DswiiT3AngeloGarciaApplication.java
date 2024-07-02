9e7c8da81bba62463358dbe861a670c9165a2bfa
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-07-02 13:50:08 -0500

    primer commit iniciando examen :3

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_AngeloGarcia/DswiiT3AngeloGarciaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_AngeloGarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3AngeloGarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3AngeloGarciaApplication.class, args);
+	}
+
+}
